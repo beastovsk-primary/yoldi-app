4a277bf1244979a798049b40e028771858abf0cc
Artyom Narshinov <narshinovartyom@mail.ru>
2023-01-30 20:58:28 +0400

    header, adaptive

--- components/UI/Header/Header.tsx
+++ components/UI/Header/Header.tsx
@@ -1,11 +1,38 @@
+import Image from "next/image";
 import Link from "next/link";
 import React, { FC } from "react";
 import s from "./Header.module.scss";
 
+import useMedia from "use-media";
+
+import logoImage from "../../../public/header/logo.svg";
+
 interface HeaderProps {}
 
 const Header: FC<HeaderProps> = () => {
-	return <div className={s.container}>Header</div>;
+	// Library for screen width, SSR support
+	const isMobile = useMedia({ maxWidth: "768px" }, true);
+
+	return (
+		<div className={s.container}>
+			<div className={s.wrapper}>
+				<Link href={""}>
+					<Image src={logoImage} alt="" />
+				</Link>
+
+				{!isMobile ? (
+					<h2 className={s.info}>
+						Разрабатываем и запускаем <br />
+						сложные веб проекты
+					</h2>
+				) : null}
+			</div>
+
+			<Link href="/auth" className={s.button}>
+				Войти
+			</Link>
+		</div>
+	);
 };
 
 export default Header;
