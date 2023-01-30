fadadf990614683faa82fa9303d7f86df079cf01
Artyom Narshinov <narshinovartyom@mail.ru>
2023-01-30 21:19:47 +0400

    footer, adaptive

--- components/UI/Footer/Footer.tsx
+++ components/UI/Footer/Footer.tsx
@@ -1,10 +1,51 @@
-import React, { FC } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { FC, useEffect, useState } from "react";
 import s from "./Footer.module.scss";
 
 interface FooterProps {}
 
 const Footer: FC<FooterProps> = () => {
-	return <div className={s.container}>Footer</div>;
+	const router = useRouter();
+	const [pageInfo, setPageInfo] = useState({ info: "", href: "", link: "" });
+
+	// destructurization  of state
+	const { info, href, link } = pageInfo;
+
+	const checkCurrentPage = () => {
+		if (!router.pathname) return;
+
+		if (router.pathname == "/reg") {
+			return setPageInfo({
+				info: "Уже есть аккаунт?",
+				href: "/auth",
+				link: "Войти",
+			});
+		}
+
+		setPageInfo({
+			info: "Еще нет аккаунта?",
+			href: "/reg",
+			link: "Зарегистрироваться",
+		});
+	};
+
+	useEffect(() => {
+		checkCurrentPage();
+	}, []);
+
+	return (
+		<div className={s.container}>
+			<div className={s.wrapper}>
+				<span className={s.info}>
+					{info}{" "}
+					<Link href={href} className={s.link}>
+						{link}
+					</Link>
+				</span>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
---
+++ pages/reg.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/components/Layout/Layout";
+import Head from "next/head";
+
+export default function Reg() {
+	return (
+		<Layout>
+			<div>Reg</div>
+		</Layout>
+	);
+}
